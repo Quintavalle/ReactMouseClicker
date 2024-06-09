ba150af08ed27a01c68befc01b327e0012439cbe
Giorgio <147600523+Quintavalle@users.noreply.github.com>
2024-06-09 07:33:27 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MouseClicker from './MouseClicker';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <MouseClicker name="one" />
+      </div>
+    );
+  }
+}
+
+export default App;
